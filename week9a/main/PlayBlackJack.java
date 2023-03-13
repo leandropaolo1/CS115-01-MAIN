668178bba83e7981e4055b04e26b7b3b97adda4d
lpcooper <pcoper79@gmail.com>
2023-03-13 10:42:07 -0600

    commit

--- week9a/main/PlayBlackJack.java
+++ week9a/main/PlayBlackJack.java
@@ -1,8 +1,8 @@
 /*
  * Student: Leandro Cooper
  * Teacher: Sister Barbara Chamberlin
- * Description: We have added The comparable  interface to Dessert Item and also added a method that compares DessertItem object
- * Lesson Learned: In this lab, I learned to add the Comparable iterface and compare and how to arrange them
+ * Description: MAin function for shwoing the balckjack card
+ * Lesson Learned: In this lab, I learned to create java applications
  * Class: 115-01
  * Date: 12-MAR-2023
  * Assignment: 9a
@@ -16,8 +16,34 @@ import week9.*;
 
 public class PlayBlackJack {
     public static void main(String[] args) {
-        BJCard ace = new BJCard(1, 2);
-        CardTest cardTest = new CardTest(ace);
-        cardTest.run(ace);
+        BJCard card1 = new BJCard(1, 1);
+        BJCard card2 = new BJCard(6, 2);
+
+        CardTest cardTest = new CardTest(card1);
+        cardTest.run(card1);
+
+        CardTest cardTest2 = new CardTest(card2);
+        cardTest2.run(card2);
+
+
+
+        BJCard card3 = new BJCard(10, 1);
+        BJCard card4 = new BJCard(14, 2);
+
+        CardTest cardTest3 = new CardTest(card3);
+        cardTest.run(card3);
+
+        CardTest cardTest4 = new CardTest(card4);
+        cardTest2.run(card4);
+
+
+        BJCard card5 = new BJCard(13, 3);
+        BJCard card6 = new BJCard(12, 4);
+
+        CardTest cardTest5 = new CardTest(card5);
+        cardTest.run(card5);
+
+        CardTest cardTest6 = new CardTest(card6);
+        cardTest2.run(card6);
     }
 }
