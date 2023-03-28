af2c5c4763f17c0a0e9222d2d9462bf081b2fba9
Koops <45190315+leandropaolo1@users.noreply.github.com>
2023-03-28 10:01:44 -0600

    Update Biddable.java

--- week11i/week11/Biddable.java
+++ week11i/week11/Biddable.java
@@ -1,3 +1,18 @@
+/*
+ * Student: Leandro Cooper
+ * Teacher: Sister Barbara Chamberlin
+ * Lesson Learned:
+I  created an interface and a class that implements the interface to store the list of residential properties that our Real Estate Office has listed for sale.
+    This will allow us to keep track of all the properties that we are currently under contract to sell. Also you can now make bids on properties and its really
+    cool
+    
+    This class is the interface that allows for bids and stuff
+ * Class: 115-01
+ * Date: 28-MAR-2023
+ * Assignment: 11i
+ * 
+ */
+
 package week11;
 
 import java.util.HashMap;
